c5c6e09b89e046b392d0f22d6c845ca8e1ceb844
Derek Simpson <articunolies22@gmail.com>
2024-06-07 17:03:43 -0700

    Initial Repo

---
+++ main.js
@@ -0,0 +1,28 @@
+// debug with extreme prejudice
+"use strict"
+
+let config = {
+	parent: 'phaser-game',
+	type: Phaser.WEBGL,
+	fps: { forceSetTimeOut: true, target: 60 },
+	render: {
+		pixelArt: true  // prevent pixel art from getting blurred when scaled
+	},
+	width: 16 * 70,
+	height: 9 * 70,
+	autoCenter: true,
+	physics: {
+		default: "arcade",
+		arcade: {
+			gravity: {
+				x: 0,
+				y: 0
+			},
+			debug: true
+		}
+	},
+	scene: [Load, MainMenu, MainGame]
+}
+
+const SCALE = 2;
+const game = new Phaser.Game(config);
